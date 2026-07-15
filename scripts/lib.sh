3d1910080aaa9a84ecee2f0abb2daa3825dfc085
maybetree <maybetree48@proton.me>
2026-07-15 13:04:14 +0200

    consolidate script

--- scripts/lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-eecho() { echo "$@" >&2 ; }
-die() { eecho "$@"; exit 1; }
-edie() { die "Failed to: $*"; }
-cd_ass() { cd "$1" || die "Failed to cd into ${2:-$1}" ; }
-ass() { "$@" || die "Failed to run command: $*" ; }
-
